95544ac5d9c210dbca8daa1bcbcc485293aae8bf
Kyle John <njk230@gmail.com>
2017-05-11 20:42:43 +0900

    Add project files.

---
+++ DSMStudentCouncil/DSMStudentCouncil/Pages/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DSMStudentCouncil.Pages
+{
+    public partial class MainPage : MasterDetailPage
+    {
+        public static string[] ListViewItems = 
+        {
+            "Open or Closed", 
+            "Labels"
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
